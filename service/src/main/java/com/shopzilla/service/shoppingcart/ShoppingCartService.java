f67e2ba099e0a7574530488c70a3bdbb4e683ea4
Jason Badua <jbadua@ucla.edu>
2014-11-07 11:47:00 -0800

    Adds initial function to load reviews from XML

--- service/src/main/java/com/shopzilla/service/shoppingcart/ShoppingCartService.java
+++ service/src/main/java/com/shopzilla/service/shoppingcart/ShoppingCartService.java
@@ -19,6 +19,10 @@ import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
 import org.skife.jdbi.v2.DBI;
 import org.skife.jdbi.v2.Handle;
+import javax.xml.parsers.*;
+import org.xml.sax.*;
+import org.w3c.dom.*;
+import java.io.IOException;
 
 /**
  * Main entry point for shopping cart service.
@@ -49,6 +53,7 @@ public class ShoppingCartService extends Service<ShoppingCartServiceConfiguratio
                 "h2");
         Handle handle = jdbi.open();
         handle.execute("create table if not exists shopping_cart_entry (shopper_id integer not null,product_id integer not null,product_name varchar(255) not null,product_cost integer not null,primary key (shopper_id, product_id))");
+        //handle.execute("create table if not exists reviews (pid integer not null,title varchar(50) not null, rating integer not null, comment varchar(255) not null,primary key (shopper_id, product_id))");
 
         /*
          * "real" database DAO
@@ -74,4 +79,55 @@ public class ShoppingCartService extends Service<ShoppingCartServiceConfiguratio
         environment.addResource(new VersionResource());
     }
 
+    public boolean loadReviews(Handle handle) {
+        Document dom;
+        // Make an instance of the DocumentBuilderFactory
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+            // use the factory to take an instance of the document builder
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            // parse using the builder to get the DOM mapping of the XML file
+            dom = db.parse("reviews.xml");
+
+            String pid = null;
+            String title = null;
+            String rating = null;
+            String comment = null;
+
+            Node product, review;
+            NodeList products = dom.getFirstChild().getChildNodes();
+
+            for (int i = 0; i < products.getLength(); i++) {
+                product = products.item(i);
+                if (product.getNodeType() == Node.ELEMENT_NODE) {
+                    NamedNodeMap nnm = product.getAttributes();
+                    pid = nnm.getNamedItem("PID").getNodeValue();
+                    Node child = product.getChildNodes().item(1);
+                    NodeList reviews = child.getChildNodes();
+
+                    for (int j = 0; j < reviews.getLength(); j++) {
+                        review = reviews.item(j);
+                        if (review.getNodeType() == Node.ELEMENT_NODE) {
+                            NamedNodeMap nnm2 = review.getAttributes();
+                            title = nnm2.getNamedItem("Title").getNodeValue();
+                            comment = nnm2.getNamedItem("Value").getNodeValue();
+                            rating = nnm2.getNamedItem("Value").getNodeValue();
+                            System.out.println(pid + title + rating + comment);
+                        }
+                    }
+                }
+            }
+
+            return true;
+
+        } catch (ParserConfigurationException pce) {
+            System.out.println(pce.getMessage());
+        } catch (SAXException se) {
+            System.out.println(se.getMessage());
+        } catch (IOException ioe) {
+            System.err.println(ioe.getMessage());
+        }
+
+        return false;
+    }
 }

3fa6e82bf80a00361c1f9e72b9894a501168d50d
Venudhar Ravishankar <venu.ravishankar@gmail.com>
2014-11-07 16:59:27 -0800

    Import dataset2.xml (product id, category, name) into h2

--- service/src/main/java/com/shopzilla/service/shoppingcart/ShoppingCartService.java
+++ service/src/main/java/com/shopzilla/service/shoppingcart/ShoppingCartService.java
@@ -4,6 +4,9 @@
  */
 package com.shopzilla.service.shoppingcart;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 import com.google.common.collect.Lists;
 import com.shopzilla.service.shoppingcart.data.ShoppingCartDao;
 import com.shopzilla.service.shoppingcart.resource.*;
@@ -15,6 +18,7 @@ import com.yammer.dropwizard.jdbi.DBIFactory;
 import com.yammer.dropwizard.jdbi.bundles.DBIExceptionsBundle;
 import com.yammer.dropwizard.views.ViewBundle;
 import com.yammer.metrics.util.DeadlockHealthCheck;
+
 import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
 import org.skife.jdbi.v2.DBI;
@@ -49,6 +53,11 @@ public class ShoppingCartService extends Service<ShoppingCartServiceConfiguratio
                 "h2");
         Handle handle = jdbi.open();
         handle.execute("create table if not exists shopping_cart_entry (shopper_id integer not null,product_id integer not null,product_name varchar(255) not null,product_cost integer not null,primary key (shopper_id, product_id))");
+        
+        /*
+         * importing dataset2.xml
+         */
+        importProductData(handle);
 
         /*
          * "real" database DAO
@@ -73,5 +82,31 @@ public class ShoppingCartService extends Service<ShoppingCartServiceConfiguratio
         environment.addResource(new IndexResource());
         environment.addResource(new VersionResource());
     }
+    
+    void importProductData(Handle handle) throws IOException {
+    	handle.execute("create table if not exists products (product_id bigint not null,product_category varchar(255) not null,product_name varchar(255) not null,primary key (product_id))");
+    	handle.execute("delete from products");
+    	RandomAccessFile raf = new RandomAccessFile("products.xml", "r");
+    	try {
+    		for (String s = raf.readLine(); s != null; s=raf.readLine()) {
+				if (s.equals("<doc>")) {
+					long pid = Long.parseLong(fieldFromLine(raf.readLine()));
+					String cat = fieldFromLine(raf.readLine()).replaceAll("'", "''");
+					String name = fieldFromLine(raf.readLine()).replaceAll("'", "''");
+					
+					
+					handle.execute("insert into products values (" + pid + ", \'" + cat + "\', \'" + name + "\')");
+				}
+    		}
+    	} finally {
+    		raf.close();
+    	}
+    }
+    
+    String fieldFromLine(String xmlLine) {
+		int start = xmlLine.indexOf(">")+1;
+		int end = xmlLine.indexOf("<", start);
+		return xmlLine.substring(start, end);
+	}
 
 }
